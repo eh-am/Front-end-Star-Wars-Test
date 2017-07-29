ff6f90a0ccc8f37b925f21dc0d8c2dacd85de8d6
eh-am <eduardoaleixomartins@gmail.com>
2017-07-29 16:10:19 -0300

    Initial commit. Folder structure and basic angular2 components working.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { CatalogComponent } from './catalog/catalog.component';
+
+import { MdButtonModule } from '@angular/material';
+import { MdListModule } from '@angular/material';
+import { MdToolbarModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MdButtonModule,
+    MdListModule,
+    MdToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
